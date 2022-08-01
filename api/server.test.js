e8d42943c5334bbc89f026e1f01b4d61d80018df
Joseph Watkins <josephxwatkins@gmail.com>
2022-08-01 18:27:55 -0400

    2 tests per endpoint, all passing

--- api/server.test.js
+++ api/server.test.js
@@ -23,22 +23,34 @@ test('is NODE_ENV set correctly?', () => {
 });
 
 describe('HTTP endpoints', () => {
-  test('POST api/auth/register', async () => {
-    let result = await request(server).post('/api/auth/register').send({ username: "Captain America", password: "baz" });
-    expect(result.status).toBe(201);
-    result = await request(server).post('/api/auth/register').send({ username: "Foo" });
-    expect(result.body).toMatchObject({ message: "username and password required" });
+  describe('[POST] /api/auth/register', () => {
+    test('[1] status when new user created', async () => {
+      let result = await request(server).post('/api/auth/register').send({ username: "Captain America", password: "baz" });
+      expect(result.status).toBe(201);
+    });
+    test('[2] error message if password missing', async () => {
+      let result = await request(server).post('/api/auth/register').send({ username: "Foo" });
+      expect(result.body).toMatchObject({ message: "username and password required" });
+    });
   });
-
-  test('POST /login', async () => {
-    let result = await request(server).post('/api/auth/login').send({ username: "Captain Marvel", password: "foo" });
-    expect(result.body).toMatchObject({ message: 'invalid credentials' });
-    result = await request(server).post('/api/auth/login').send({ username: "foobar" });
-    expect(result.body).toMatchObject({ message: "username and password required" });
+  describe('[POST] /api/auth/login', () => {
+    test('[1] error message for invalid credentials', async () => {
+      let result = await request(server).post('/api/auth/login').send({ username: "Captain Marvel", password: "foo" });
+      expect(result.body).toMatchObject({ message: 'invalid credentials' });
+    });
+    test('[2] error message if username is missing', async () => {
+      let result = await request(server).post('/api/auth/login').send({ password: "foobar" });
+      expect(result.body).toMatchObject({ message: "username and password required" });
+    });
   });
-
-  test('GET /jokes', async () => {
-    let result = await request(server).get('/api/jokes');
-    expect(result.body).toMatchObject({ message: "token required" });
+  describe('[GET] /api/jokes', () => {
+    test('[1] error if no token received', async () => {
+      let result = await request(server).get('/api/jokes');
+      expect(result.body).toMatchObject({ message: "token required" });
+    });
+    test('[2]', async () => {
+      let result = await request(server).get('/api/jokes').set({authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWJqZWN0IjoxLCJ1c2VybmFtZSI6IkNhcHRhaW4gTWFydmVsIiwiaWF0IjoxNjU5MzkyNzM0LCJleHAiOjE2NTk0NzkxMzR9.V0KKEA9IRFFL9RHfRL-PvJyK31mi8swAFBpYjRMQVZI'});
+      expect(result.body).toHaveLength(3)
+    });
   });
 });
